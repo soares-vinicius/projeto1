0e94a7402ae22a0408631a36129673ec6f1a4869
soares-vinicius <soares.vinicius.andrade@gmail.com>
2023-10-19 19:57:01 -0300

    🎉 Inicio do projeto

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css"
+import HomePage from "./Pages/Home/index"
+
+function App() {
+  return (
+    <div
+      className="App">
+      <HomePage/>
+    </div>
+  );
+}
+
+export default App;
